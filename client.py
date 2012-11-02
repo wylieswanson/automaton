07d924e5fc4cf8291429b165585cb868973a08c8
Wylie Swanson <wylie@rpigreen.(none)>
2012-11-02 13:02:39 -0700

    moved an example to a function, for keeping

--- client.py
+++ client.py
@@ -3,43 +3,46 @@
 import jsonrpclib, time
 from pprint import pprint
 
-server = jsonrpclib.Server("http://localhost:8080")
+zwave = jsonrpclib.Server("http://localhost:8080")
 
-#print server.ozw_library_version(),
-#print server.python_ozw_library_version(),
-#print server.zw_library_version(),
-#print server.home_id()
-#print server.controller_node_id(),
-#print server.controller_node_version(),
-#print server.nodes()
-#print server.controller_capabilities() # error	
-#print server.controller_node_capabilities() # error
-#print server.controller_stats()
+def switchinfo():
+	locations = sorted (zwave.all_switch_locations() )
+	pprint(locations)
+	for location in locations:
+		switches = sorted (zwave.all_switches_in_location(location) )
+		num_switches_on = 0
+		num_switches_off = 0
+		for switch in switches:
+			if switch['state']: num_switches_on += 1
+			else: num_switches_off += 1
+			# pprint(switch)
+		print "%s: on=%s, off=%s" % (location, num_switches_on,num_switches_off)
 
-#server.print_nodes()
-#server.print_all_lights()
-locations = sorted (server.all_switch_locations() )
-pprint(locations)
-for location in locations:
-	switches = sorted (server.all_switches_in_location(location) )
-	num_switches_on = 0
-	num_switches_off = 0
-	for switch in switches:
-		if switch['state']: num_switches_on += 1
-		else: num_switches_off += 1
+#print zwave.ozw_library_version(),
+#print zwave.python_ozw_library_version(),
+#print zwave.zw_library_version(),
+#print zwave.home_id()
+#print zwave.controller_node_id(),
+#print zwave.controller_node_version(),
+#print zwave.nodes()
+#print zwave.controller_capabilities() # error	
+#print zwave.controller_node_capabilities() # error
+#print zwave.controller_stats()
 
-		# pprint(switch)
-	print "%s: on=%s, off=%s" % (location, num_switches_on,num_switches_off)
+#zwave.print_nodes()
+#zwave.print_all_lights()
 
-#server.all_lights_off()
+switchinfo()
+
+#zwave.all_lights_off()
 #time.sleep(1.0)
-#server.all_lights_on()
+#zwave.all_lights_on()
 
-#print server.test()
+#print zwave.test()
 
-#server.light_on(2)
+#zwave.light_on(2)
 #time.sleep(3.0)
-server.light_off(8)
+zwave.light_off(8)
 
-#print server.get_switch_state(3)
-#print server.get_switch_state(7)
+#print zwave.get_switch_state(3)
+#print zwave.get_switch_state(7)
